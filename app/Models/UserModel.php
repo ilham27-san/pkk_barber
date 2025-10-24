2cb8e3a097b8ffc824dbf42166115d7971dff999
unknown <aliefdzulfikar55@gmail.com>
2025-10-24 21:51:15 +0700

    Perbaiki login

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,8 +1,20 @@
-<?php namespace App\Models;
+<?php
+
+namespace App\Models;
+
 use CodeIgniter\Model;
 
-class UserModel extends Model {
+class UserModel extends Model
+{
+    // [FIX 1] Ini untuk mengatasi error 'updated_at'
+    protected $useTimestamps = false;
+
+    // [FIX 2] Pastikan nama tabelnya benar
     protected $table = 'users';
-    protected $allowedFields = ['username','email','password','role'];
-    protected $useTimestamps = true;
+
+    // [FIX 3 - PALING PENTING]
+    // Pastikan 'password' ada di dalam array ini!
+    protected $allowedFields = ['username', 'email', 'password', 'role'];
+
+    // ... sisa kode model Anda (jika ada) ...
 }
