c2f3fce78bbc67fe4e2c3a2b2ceef47f58371b0c
unknown <aliefdzulfikar55@gmail.com>
2025-11-04 22:38:12 +0700

    perbaiki gallery.php

--- app/Views/page/gallery.php
+++ app/Views/page/gallery.php
@@ -4,7 +4,7 @@
 <section class="gallery">
     <h2>Gallery</h2>
     <div class="gallery-grid">
-        <img src="<?= base_url('assets/images/gallery1.jpg'); ?>" alt="Barbershop Interior">
+        <img src="<?= base_url('assets/images/gallery1.png'); ?>" alt="Barbershop Interior">
         <img src="<?= base_url('assets/images/gallery2.jpg'); ?>" alt="Haircut Style">
         <img src="<?= base_url('assets/images/gallery3.jpg'); ?>" alt="Barber Team">
     </div>
